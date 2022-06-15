f128027b6d858209751169d68f4d1f8adcad8116
arthurtng <darkmatter2000@gmail.com>
2022-06-15 17:11:36 +0100

    colliders movement

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Mover : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10;
+    private HashSet<char> LockedDirections; // Record which directions are locked.
+    private char currentDirection; // Record which direction just went.
+
+    [SerializeField] private Tilemap groundTileMap;
+    [SerializeField] private Tilemap collisionTileMap;
+
+    //public List<Vector3> tileWorldLocations;
 
     // Start is called before the first frame update
     void Start()
     {
         PrintInstructions();
+        LockedDirections = new HashSet<char>();
+        //tileLook();
     }
 
     // Update is called once per frame
@@ -26,7 +36,7 @@ public class Mover : MonoBehaviour
 
     //void MovePlayer()
     //{
-        
+
     //    float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
     //    float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
     //    int x = (int)xValue;
@@ -45,31 +55,162 @@ public class Mover : MonoBehaviour
 
     public void MoveLeft()
     {
-        transform.Translate(-0.1f, 0.0f, 0.0f);
+        if (!LockedDirections.Contains('l'))
+        {
+            transform.Translate(-0.1f, 0.0f, 0.0f);
+            currentDirection = 'l';
+            LockedDirections.Remove('r');
+        }
     }
 
     public void MoveRight()
     {
-        transform.Translate(0.1f, 0.0f, 0.0f);
+        if (!LockedDirections.Contains('r'))
+        {
+            transform.Translate(0.1f, 0.0f, 0.0f);
+            currentDirection = 'r';
+            LockedDirections.Remove('l');
+        }
     }
 
     public void MoveUp()
     {
-        transform.Translate(0f, 0f, 0.1f);
+        if (!LockedDirections.Contains('u'))
+        {
+            transform.Translate(0f, 0f, 0.1f);
+            currentDirection = 'u';
+            LockedDirections.Remove('d');
+        }
     }
 
     public void MoveDown()
     {
-        transform.Translate(0f, 0f, -0.1f);
+        if (!LockedDirections.Contains('d'))
+        {
+            transform.Translate(0f, 0f, -0.1f);
+            currentDirection = 'd';
+            LockedDirections.Remove('u');
+        }
     }
 
-    bool isOnGround(float xVal, float zVal)
+    //public void MoveLeft()
+    //{
+    //    if (checkMove(new Vector3(-1f, -1.5f, 0.0f)))
+    //    {
+    //        transform.Translate(-0.1f, 0.0f, 0.0f);
+    //        currentDirection = 'l';
+    //        LockedDirections.Remove('r');
+    //    }
+    //}
+
+    //public void MoveRight()
+    //{
+    //    if (!LockedDirections.Contains('r'))
+    //    {
+    //        transform.Translate(0.1f, 0.0f, 0.0f);
+    //        currentDirection = 'r';
+    //        LockedDirections.Remove('l');
+    //    }
+    //}
+
+    //public void MoveUp()
+    //{
+    //    if (!LockedDirections.Contains('u'))
+    //    {
+    //        transform.Translate(0f, 0f, 0.1f);
+    //        currentDirection = 'u';
+    //        LockedDirections.Remove('d');
+    //    }
+    //}
+
+    //public void MoveDown()
+    //{
+    //    if (!LockedDirections.Contains('d'))
+    //    {
+    //        transform.Translate(0f, 0f, -0.1f);
+    //        currentDirection = 'd';
+    //        LockedDirections.Remove('u');
+    //    }
+    //}
+
+    //bool isOnGround(float xVal, float zVal)
+    //{
+    //    if ((xVal >= 0 && xVal <= 5) && (zVal >= 0 && zVal <= 5))
+    //    {
+    //        return true;
+    //    }
+    //    return false;
+    //}
+
+    private void OnTriggerEnter(Collider other)
     {
-        if ((xVal >= 0 && xVal <= 5) && (zVal >= 0 && zVal <= 5))
+        if (other.CompareTag("Wall"))
         {
-            return true;
+            LockedDirections.Add(currentDirection);
         }
-        return false;
+    }
+
+    private bool checkMove(Vector3 direction)
+    {
+        Vector3Int gridPosition = groundTileMap.WorldToCell(transform.localPosition + direction);
+
+        if (!groundTileMap.HasTile(gridPosition))
+        {
+            Debug.Log("No ground tile");
+            return false;
+        }
+        if (collisionTileMap.HasTile(gridPosition))
+        {
+            Debug.Log("Col");
+            return false;
+        }
+        return true;
+
+    }
+
+    //private void tileCheck()
+    //{
+    //    BoundsInt bounds = groundTileMap.cellBounds;
+    //    TileBase[] allTiles = groundTileMap.GetTilesBlock(bounds);
+
+    //    for (int x = 0; x < bounds.size.x; x++)
+    //    {
+    //        for (int y = 0; y < bounds.size.z; y++)
+    //        {
+    //            TileBase tile = allTiles[x + y * bounds.size.x];
+    //            if (tile != null)
+    //            {
+    //                Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
+    //            }
+    //            else
+    //            {
+    //                Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+    //            }
+    //        }
+    //    }
+    //}
+
+    private void tileLook()
+    {
+        //tileWorldLocations = new List<Vector3>();
+
+        //foreach (var pos in groundTileMap.cellBounds.allPositionsWithin)
+        //{
+        //    Vector3Int localPlace = new Vector3Int(pos.x, pos.y, pos.z);
+        //    Vector3 place = groundTileMap.CellToWorld(localPlace);
+        //    if (groundTileMap.HasTile(localPlace))
+        //    {
+        //        tileWorldLocations.Add(place);
+        //    }
+        //}
+
+        //foreach (Vector3 i in tileWorldLocations)
+        //{
+        //    Debug.Log(i);
+        //}
+        //Debug.Log(tileWorldLocations);
+
+
     }
 
 }
