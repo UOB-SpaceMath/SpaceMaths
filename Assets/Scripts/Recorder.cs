fab0b8889656942b065ac55abac69b18f93940ad
desmond <dmz990121@outlook.com>
2022-06-15 11:57:25 +0100

    add watson lib

--- Assets/Scripts/Recorder.cs
+++ Assets/Scripts/Recorder.cs
@@ -2,10 +2,30 @@ using UnityEngine;
 
 public class Recorder : MonoBehaviour
 {
+    AudioSource m_Source;
 
-    AudioClip m_InputVoice;
-
-    public void ClickDownRecord()
+    void Start()
     {
+        m_Source = GetComponent<AudioSource>();
+    }
+
+    public void StartRecording()
+    {
+        m_Source.clip = Microphone.Start("", false, 30, 44100);
+
+    }
+
+    public void EndRecording()
+    {
+        if (Microphone.IsRecording(""))
+        {
+            Microphone.End(null);
+        }
+    }
+
+    public void PlayRecord()
+    {
+        m_Source.Play();
     }
 }
+
