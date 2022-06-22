68473d4944b9edd7b00f2d60d3b115d21aaebcee
desmond <dmz990121@outlook.com>
2022-06-22 16:59:23 +0100

    add debug camera

--- Assets/Scripts/GameBoardManager.cs
+++ Assets/Scripts/GameBoardManager.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 public class GameBoardManager : MonoBehaviour
 {
+    // The height of ship according to the game board.
     [SerializeField]
     float _height;
 
+    // the obstacle tile-map
+    [SerializeField]
+    Tilemap _obstacleMap;
+
+    // the player's ship
     [SerializeField]
     Ships _playerShip;
     public Ships PlayerShips
@@ -12,6 +18,7 @@ public class GameBoardManager : MonoBehaviour
         get => _playerShip;
     }
 
+    // the enemies' ships
     [NonReorderable]
     [SerializeField]
     Ships[] _enermyShips;
@@ -20,8 +27,13 @@ public class GameBoardManager : MonoBehaviour
         get => _enermyShips;
     }
 
-    enum CellType { Ship, Wall, Empty };
-    CellType[,] _cells;
+    // different types of cell
+    public enum CellType { Ship, Wall, Empty };
+
+    // the whole map information
+    CellType[,] _cells; // the whole map info
+
+    // the bias of a ship's position compare to the cells' position
     static readonly Vector3 _PLAYER_BIAS = new Vector3(0.5f, 0.5f, 0);
     Vector3Int _tileBias;
 
@@ -32,12 +44,12 @@ public class GameBoardManager : MonoBehaviour
         SetupShip();
     }
 
+    // scan the whole map
     void GetWallInfo()
     {
         // get map info
-        var obstacleMap = transform.Find("Grid/Obstacle Tilemap").GetComponent<Tilemap>();
-        obstacleMap.CompressBounds();
-        var obstacleBound = obstacleMap.cellBounds;
+        _obstacleMap.CompressBounds();
+        var obstacleBound = _obstacleMap.cellBounds;
         var xSize = obstacleBound.size.x - 2;
         var ySize = obstacleBound.size.y - 2;
         _cells = new CellType[xSize, ySize];
@@ -46,12 +58,13 @@ public class GameBoardManager : MonoBehaviour
         {
             for (int y = 0; y < _cells.GetLength(1); y++)
             {
-                _cells[x, y] = obstacleMap.HasTile(new Vector3Int(x, y, 0) + _tileBias) ?
+                _cells[x, y] = _obstacleMap.HasTile(new Vector3Int(x, y, 0) + _tileBias) ?
                     CellType.Wall : CellType.Empty;
             }
         }
     }
 
+    // place ships to specific cells
     void SetupShip()
     {
         bool isSetup = true;
@@ -66,6 +79,8 @@ public class GameBoardManager : MonoBehaviour
             throw new System.Exception("Fail to setup ship: the cell is not empty.");
     }
 
+    // set ship to a cell
+    // don't use this method to move the ship, use MoveShip()
     bool SetShip(Ships ship, int x, int y)
     {
         if (IsEmpty(x, y))
@@ -80,7 +95,7 @@ public class GameBoardManager : MonoBehaviour
             return false;
     }
 
-    bool MoveShip(Ships ship,int x,int y)
+    bool MoveShip(Ships ship, int x, int y)
     {
         _cells[ship.position.x, ship.position.y] = CellType.Empty;
         return SetShip(ship, x, y);
@@ -89,6 +104,11 @@ public class GameBoardManager : MonoBehaviour
     Vector3 GetPosision(int x, int y)
     {
         return new Vector3(x + _tileBias.x + _PLAYER_BIAS.x, _height, -(y + _tileBias.y + _PLAYER_BIAS.y));
+    }
+
+    Vector2Int GetPlayerIndex()
+    {
+        return _playerShip.position;
     }
 
     bool IsEmpty(int x, int y)
--- Assets/Scripts/LookCamera.cs
+++ Assets/Scripts/LookCamera.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LookCamera : MonoBehaviour
@@ -9,7 +7,19 @@ public class LookCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Camera = GameObject.Find("AR Session Origin/AR Camera");
+        // setup camera to use
+        if (Application.platform == RuntimePlatform.LinuxEditor ||
+           Application.platform == RuntimePlatform.WindowsEditor ||
+           Application.platform == RuntimePlatform.OSXEditor)
+        {
+            Camera = GameObject.Find("Debug Camera");
+        }
+        else
+        {
+            Camera = GameObject.Find("AR Session Origin/AR Camera");
+        }
+        // set camera for canvas
+        GetComponentInChildren<Canvas>().worldCamera = Camera.GetComponent<Camera>();
     }
 
     // Update is called once per frame
