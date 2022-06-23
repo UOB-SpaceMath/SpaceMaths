686d41124dbd94d0e2713f74ecb35410dcebe918
pcypoon <sb21713@bristol.ac.uk>
2022-06-23 15:14:43 +0100

    edit selection grid

--- Assets/Scripts/SelectionGridManager.cs
+++ Assets/Scripts/SelectionGridManager.cs
@@ -5,19 +5,21 @@ using UnityEngine.Tilemaps;
 
 public class SelectionGridManager : MonoBehaviour
 {
-    [SerializeField] Ships player;
+    
     [SerializeField] Tilemap selectionGrid;
-    // [SerializeField] Tilemap obstacleMap;
-
-    GameBoardManager gameBoardManager = new GameBoardManager();
-
-    // enum CellType { Ship, Wall, Empty };
-    // CellType[,] _cells;
-    Vector2Int playerPosition;
+    [SerializeField] GameObject gameBoard;
+    
+    Vector2Int playerIndex;
+    GameBoardManager.CellType[,] _cells;
+    GameBoardManager.CellType[,] _selectionCells;
 
     // Start is called before the first frame update
     void Start()
     {
+        var gameBoardManager =  gameBoard.GetComponent<GameBoardManager>();
+        _cells = gameBoardManager.Cells;
+        _selectionCells = new GameBoardManager.CellType[5,5];
+        playerIndex = gameBoardManager.PlayerShips.cellIndex;
 
         buildGrid();
     }
@@ -36,9 +38,9 @@ public class SelectionGridManager : MonoBehaviour
         
       
 
-        Vector3 WorldPos = player.shipObject.transform.position;
-        float WorldPositionX = WorldPos.x;
-        float WorldPositionY = WorldPos.y;
+        // Vector3 WorldPos = player.shipObject.transform.position;
+        // float WorldPositionX = WorldPos.x;
+        // float WorldPositionY = WorldPos.y;
 
         // Debug.Log("world Position" + obstacleMap.transform.position);
         // Debug.Log("local position" + obstacleMap.transform.localPosition);
@@ -53,22 +55,35 @@ public class SelectionGridManager : MonoBehaviour
         //     }
         // }
 
-        Vector3 localPos = player.shipObject.transform.localPosition;
-        int LocalPosX = (int)localPos.x;
-        int LocalPosZ = (int)localPos.z;
-        Debug.Log("local pos x"+ LocalPosX);
-        int [,] obstacleArray  = new int[5,5];
+        // Vector3 localPos = player.shipObject.transform.localPosition;
+   
+        
+        int localPosX = playerIndex.x;
+        int localPosY = playerIndex.y;
+        int startX = playerIndex.x - 2;
+        int startY = playerIndex.y - 2;   
+]
+         for(int x = 0; x < 5; x++){
+            for(int y = 0; y < 5; y++){
+        
+            int currentX = startX + x;
+            int currentY = startY + y;
 
-         for(int z = LocalPosZ - 2; z < LocalPosZ + 2; z++){
-            for(int x = LocalPosX -2; x < LocalPosX + 2; x++){
-                //issue with line 65
-              if(obstacleMap.HasTile(new Vector3Int(x, 0, z))){
+            if(currentX < 0 || currentY < 0 || currentX > _selectionCells.GetLength(0) || )
+            // if(_cells[x,y] == GameBoardManager.CellType.Empty){
 
-                obstacleArray[z + 2, x + 2] = 1;
-              }else{
-                obstacleArray[z + 2, x + 2] = 0;
-              }
-            }
+            // }else if()
+                
+           
+
+
+
+
+            //     obstacleArray[y + 2, y + 2] = 1;
+            //   }else{
+            //     obstacleArray[y + 2, y + 2] = 0;
+            //   }
+            
          }
 
          Debug.Log(obstacleArray.GetLength(1));
@@ -93,6 +108,12 @@ public class SelectionGridManager : MonoBehaviour
 
 
 
+    Vector2Int GetSelectionIndexFromPlayerIndex (Vector2Int playerIndex){
+        Vector2Int result = new Vector2Int(0, 0);
+        result.y = playerIndex.y - playerIndex.y - 2;
+        result.x = playerIndex.x - playerIndex.x + 2;
+        return result;
+    }
     
     // Set the colour of a tile.    
     private void SetTileColour(Color colour, Vector3Int position, Tilemap tilemap)
