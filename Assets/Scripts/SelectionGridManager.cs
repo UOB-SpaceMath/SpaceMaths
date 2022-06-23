13131f4d23efc87a5a8b91dc59138708265500f7
pcypoon <sb21713@bristol.ac.uk>
2022-06-23 16:30:47 +0100

    build selection grid

--- Assets/Scripts/SelectionGridManager.cs
+++ Assets/Scripts/SelectionGridManager.cs
@@ -5,116 +5,94 @@ using UnityEngine.Tilemaps;
 
 public class SelectionGridManager : MonoBehaviour
 {
-    
+
     [SerializeField] Tilemap selectionGrid;
     [SerializeField] GameObject gameBoard;
-    
+
+    public enum ActionType { None, Move, Attack };
     Vector2Int playerIndex;
     GameBoardManager.CellType[,] _cells;
-    GameBoardManager.CellType[,] _selectionCells;
+    ActionType[,] _selectionCells;
+
 
     // Start is called before the first frame update
-    void Start()
+
+
+
+    void OnEnable()
     {
-        var gameBoardManager =  gameBoard.GetComponent<GameBoardManager>();
+        var gameBoardManager = gameBoard.GetComponent<GameBoardManager>();
         _cells = gameBoardManager.Cells;
-        _selectionCells = new GameBoardManager.CellType[5,5];
+        _selectionCells = new ActionType[5, 5];
         playerIndex = gameBoardManager.PlayerShips.cellIndex;
 
         buildGrid();
+    }
+    void Start()
+    {
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void buildGrid()
     {
         // TODO: retrieve actual player position to determine 5x5 squares around player; at the moment player.position returns cell position
 
-       // gameBoardManager.GetWallInfo();
-        
-      
-
-        // Vector3 WorldPos = player.shipObject.transform.position;
-        // float WorldPositionX = WorldPos.x;
-        // float WorldPositionY = WorldPos.y;
-
-        // Debug.Log("world Position" + obstacleMap.transform.position);
-        // Debug.Log("local position" + obstacleMap.transform.localPosition);
-
-        // Own SelectionGrid
-        // Offset: x-coordinate from -6 to -2; y-coordinate no offset
-        // for (int y =0; y <= 4; y++)
-        // {
-        //     for (int x = -6; x <= -2; x++)
-        //     {
-        //         SetTileColour(Color.green, new Vector3Int(x, y, 0), selectionGrid);
-        //     }
-        // }
-
-        // Vector3 localPos = player.shipObject.transform.localPosition;
-   
-        
-        int localPosX = playerIndex.x;
-        int localPosY = playerIndex.y;
-        int startX = playerIndex.x - 2;
-        int startY = playerIndex.y - 2;   
-]
-         for(int x = 0; x < 5; x++){
-            for(int y = 0; y < 5; y++){
-        
-            int currentX = startX + x;
-            int currentY = startY + y;
-
-            if(currentX < 0 || currentY < 0 || currentX > _selectionCells.GetLength(0) || )
-            // if(_cells[x,y] == GameBoardManager.CellType.Empty){
-
-            // }else if()
-                
-           
+        Debug.Log(playerIndex.x);
+        Debug.Log(playerIndex.y);
+        int startX = playerIndex.x + 2;
+        int startY = playerIndex.y - 2;
+        int selectionX = 0;
+        int selectionY = 0;
 
 
+        for (int x = startX; x < startX - 5; x--)
+        {
+            for (int y = startY; y < startY + 5; y++)
+            {
 
+                if (x < 0 || y < 0 || x >= _cells.GetLength(0) || y >= _cells.GetLength(1) ||
+                _cells[x, y] == GameBoardManager.CellType.Wall)
+                {
+                    _selectionCells[x, y] = ActionType.None;
+                }
+                else if (_cells[x, y] == GameBoardManager.CellType.Empty)
+                {
+                    _selectionCells[selectionX, selectionY] = ActionType.Move;
+                }
+                else if (_cells[x, y] == GameBoardManager.CellType.Ship)
+                {
+                    _selectionCells[selectionX, selectionY] = ActionType.Attack;
+                }
 
-            //     obstacleArray[y + 2, y + 2] = 1;
-            //   }else{
-            //     obstacleArray[y + 2, y + 2] = 0;
-            //   }
-            
-         }
-
-         Debug.Log(obstacleArray.GetLength(1));
-
-         for(int j = 0; j< obstacleArray.GetLength(0); j++){
-            for(int k = 0; k < obstacleArray.GetLength(1); k++){
-                Debug.Log(j);
-                Debug.Log(k);
-               if(obstacleArray[j,k] ==0){
-                // SetTileColour()
-                
-               }else{
-
-               }
+                selectionY++;
             }
-         }
+            selectionX++;
+        }
 
-        // Player position is centred on our Selection Grid
-        Vector3Int playerPositionInV3 = new Vector3Int(-4, 2, 0);       
-        SetTileColour(Color.red, playerPositionInV3, selectionGrid);
+
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Debug.Log("Cell type"+ _selectionCells[i, j]);
+            }
+        }
+
     }
 
+    // private Vector2Int GetSelectionIndex(Vector2Int playerIndex, Vector2Int cellIndex)
+    // {
 
+    // }
 
-    Vector2Int GetSelectionIndexFromPlayerIndex (Vector2Int playerIndex){
-        Vector2Int result = new Vector2Int(0, 0);
-        result.y = playerIndex.y - playerIndex.y - 2;
-        result.x = playerIndex.x - playerIndex.x + 2;
-        return result;
-    }
-    
     // Set the colour of a tile.    
     private void SetTileColour(Color colour, Vector3Int position, Tilemap tilemap)
     {
@@ -125,8 +103,8 @@ public class SelectionGridManager : MonoBehaviour
         // Set the colour.
         tilemap.SetColor(position, colour);
     }
-
-    
 }
+
+
 
 
