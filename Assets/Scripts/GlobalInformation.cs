ebb59092e819cede72bb5209b257c30cc7ff5738
desmond <dmz990121@outlook.com>
2022-07-18 13:20:05 +0100

    make levels to be prefabs

---
+++ Assets/Scripts/GlobalInformation.cs
@@ -0,0 +1,8 @@
+﻿namespace SpaceMath
+{
+    // all global information will be here
+    public static class GlobalInformation
+    {
+        public static int CurrentLevelIndex { get; set; }
+    }
+}
