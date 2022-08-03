251a7d1b4eeee7068682eec54a8a471788650f81
Jimmy-True <chufangxu@gmail.com>
2022-08-03 20:05:22 +0100

    Debugging

--- Assets/Scripts/AttackManager.cs
+++ Assets/Scripts/AttackManager.cs
@@ -9,6 +9,16 @@ public class AttackManager : MonoBehaviour
     private AudioSource attackAudio;
     private LineRenderer attackLine;
 
+    [SerializeField]
+    private float rotationSpeed;
+    private float rotationFrames = 120;
+
+    [SerializeField]
+    private GameObject DeathExplosionPrefab;
+    [SerializeField]
+    private GameObject AttackExplosionPrefab;
+    private AudioSource explosionAudio;
+
     // Call this method to attack.
     public bool Attack(Ships attacker, Ships victim)
     {
@@ -44,10 +54,22 @@ public class AttackManager : MonoBehaviour
     // Turn on the audio and visual effects of attack, and keep the visual effect for a set time.
     private IEnumerator AttackEffect(Ships attacker, Ships victim, RaycastHit hit)
     {
+        /* Rotation */
         Vector3 targetDirection = victim.ShipObject.transform.position - attacker.ShipObject.transform.position;
         targetDirection.y = 0;
-        attacker.ShipObject.transform.rotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+        /* No effects */
+        //Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        //attacker.ShipObject.transform.rotation = Quaternion.Slerp(attacker.ShipObject.transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
 
+        for (int i = 1; i <= rotationFrames; i++)
+        {
+            Debug.Log(i * targetDirection / rotationFrames);
+            attacker.ShipObject.transform.rotation = Quaternion.LookRotation(i * targetDirection / rotationFrames, Vector3.up);
+            /* No effects */
+            //yield return null;
+        }
+
+        /* Attacking effects */
         attackLine.enabled = true;
         attackAudio = attacker.ShipObject.GetComponent<AudioSource>();
         if(attackAudio != null)
@@ -57,7 +79,25 @@ public class AttackManager : MonoBehaviour
 
         if (hit.collider.CompareTag("Enemy") || hit.collider.CompareTag("Player"))
         {
-            victim.ApplyDamage(attacker.AttackDamage);
+            Vector3 explosionPosition = victim.ShipObject.transform.position;
+            GameObject explosion;
+            /* If victim was destroyed, trigger a bigger explosion */
+            if (victim.ApplyDamage(attacker.AttackDamage) == 1)
+            {
+                explosion = Instantiate<GameObject>(DeathExplosionPrefab);
+                explosion.transform.position = explosionPosition;
+                explosionAudio = GetComponent<AudioSource>();
+                explosionAudio.Play();
+                yield return new WaitForSeconds(1.0f);
+                Destroy(explosion);
+            }
+            else
+            {
+                explosion = Instantiate<GameObject>(AttackExplosionPrefab);
+                explosion.transform.position = explosionPosition;
+                yield return new WaitForSeconds(0.5f);
+                Destroy(explosion);
+            }
         }
 
         yield return new WaitForSeconds(0.5f);
