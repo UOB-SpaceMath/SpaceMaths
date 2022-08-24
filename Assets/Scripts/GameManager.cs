840c061a5a691b40ae5792ad0040400735244cdd
Jimmy-True <chufangxu@gmail.com>
2022-08-24 15:11:35 +0100

    Renamed UIManager 2 QuestionManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour
     // Sub managers
     [Header("Managers")] [SerializeField] private AttackManager _am;
     [SerializeField] private GameBoardManager _gbm;
-    [SerializeField] private UIManager _uim;
+    [SerializeField] private QuestionManager _qm;
     [SerializeField] private SelectionGridManager _sgm;
     [SerializeField] private WatsonManager _wm;
     [SerializeField] private MessageManager _mm;
@@ -77,20 +77,20 @@ public class GameManager : MonoBehaviour
                 // Question stage
                 case Stages.Question:
                     SetPanel(PanelType.Question);
-                    switch (_uim.GetAnswerState())
+                    switch (_qm.GetAnswerState())
                     {
                         // Answer was correct
-                        case UIManager.AnswerStates.Right:
+                        case QuestionManager.AnswerStates.Right:
                             _stage = Stages.None;
-                            _uim.SetAnswerState(UIManager.AnswerStates.Suspension);
+                            _qm.SetAnswerState(QuestionManager.AnswerStates.Suspension);
                             StartCoroutine(QuestionToPlayerTurn());
                             _scores.incrementScore();
                             break;
 
                         // Answer was incorrect
-                        case UIManager.AnswerStates.Wrong:
+                        case QuestionManager.AnswerStates.Wrong:
                             _stage = Stages.None;
-                            _uim.SetAnswerState(UIManager.AnswerStates.Suspension);
+                            _qm.SetAnswerState(QuestionManager.AnswerStates.Suspension);
                             StartCoroutine(QuestionToEnemiesTurn());
                             break;
                     }
--- Assets/Scripts/QuestionManager.cs
+++ Assets/Scripts/QuestionManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class UIManager : MonoBehaviour
+public class QuestionManager : MonoBehaviour
 {
     // TODO remove this.
     [SerializeField]
